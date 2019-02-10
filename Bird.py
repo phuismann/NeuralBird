7e2ed84938f55eafd1329b4b038f45b6cef9a1e4
Philipp <45405932+FruitVul@users.noreply.github.com>
2019-02-10 15:14:19 +0100

    fixed poor population improvements

---
+++ Bird.py
@@ -0,0 +1,87 @@
+import sys, pygame, neat
+
+
+class bird:
+    height = 600
+    width = 400
+    gravity = 1
+    lift = -50
+    yStart = 250
+
+
+
+
+
+    def __init__(self,screen,genome,config):
+        self.x = 100
+        self.y = self.yStart
+        self.velocity = 0
+        self.screen = screen
+        self.genome = genome
+        self.neural_network = neat.nn.FeedForwardNetwork.create(genome, config)
+        self.dead = False;
+        self.distance = 0
+        self.distance_obst = 0
+        self.input = [0, 0, 0, 0,0,0]#self.input
+        self.score = 0
+    def show(self):
+        if self.dead == False:
+            pygame.draw.circle(self.screen,(255,255,255),[self.x, self.y],15)
+            #self.screen.blit(pygame.image.load('assets/redbird-downflap.png'),(self.x, self.y))
+        else:
+            pygame.draw.circle(self.screen, (0, 0, 0), [-100, -100], 15)
+    def update(self):
+        self.velocity += self.gravity
+        self.y += int(self.velocity*0.1)
+        if (self.y > int(self.height)):
+            self.y = int(self.height)
+            self.velocity = 0
+        if (self.y < 0):
+            self.y = 0
+            self.velocity = 0
+        self.score += self.distance/1000
+
+
+    def up(self):
+        self.velocity += self.lift
+        self.score-= 1 #lot of jumping no good
+
+
+    def hits(self,pipes):
+        for pipe in pipes:
+            if self.y < pipe.top or self.y > pipe.height - pipe.bottom or self.y >= self.height or self.y <= 0:
+                if self.x > pipe.x and self.x < pipe.x + pipe.w or self.y >= self.height:
+                    return True
+
+
+    def think(self,pipes):
+        min_dist = 1000000
+        closest_pipe = pipes[0]
+        pipeClose = lambda piipe:  piipe.x -self.x
+        for pipe in pipes:
+
+            if pipeClose(pipe) <= min_dist and pipeClose(pipe) > 0:
+                min_dist = pipeClose(pipe)
+                closest_pipe = pipe
+                self.distance_obst = min_dist
+
+
+
+
+        self.input[0] = self.velocity/20
+        self.input[1] = closest_pipe.x/self.width
+        self.input[2] = (self.y-closest_pipe.top)/self.height
+        self.input[3] = (closest_pipe.bottom-self.y)/self.height
+        self.input[4] = closest_pipe.mid_distance/self.width
+        self.input[5] = self.distance_obst/100 #Distance to closest pipe
+
+
+    def decide(self):
+        output = self.neural_network.activate(self.input)
+        if output[0] > 0.5:
+            self.up()
+
+
+
+
+
---
+++ Game.py
@@ -0,0 +1,86 @@
+import pygame
+import Bird
+import Pipe
+import neat
+import numpy as np
+
+def game(genomes, config, score):
+    pygame.init()
+    pygame.font.init()
+    clock = pygame.time.Clock()
+    size = width, height = 400, 600
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Birdy")
+    dead_count = 0
+    pipes = []
+    birds = []
+    genome_info=[]
+    score_info = []
+    timer = 0
+    done = False
+    game_score = 0
+
+
+
+    for genome in genomes:
+        birds.append(Bird.bird(screen=screen,genome=genome,config=config))
+
+
+    ##Neural Net
+    pipes.append(Pipe.pipe())
+    pipes[0].top =  height / 4
+    pipes[0].bottom = height / 2
+
+    while not done:
+        timer += 1
+
+        myfont = pygame.font.SysFont('Comic Sans MS', 30)
+        textsurface = myfont.render("Score: "+str(game_score), False, (255, 255, 255))
+        screen.blit(textsurface, (0, 0))
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        if timer % 150 == 0:
+            pipes.append(Pipe.pipe())
+
+        for pipe in pipes:
+            pipe.show()
+            pipe.update()
+            if pipe.offscreen() and len(pipes) > 10:
+                pipes.remove(pipe)
+
+
+
+        for index, bird in enumerate(birds):
+
+            score = bird.score
+            bird.update()
+            bird.show()
+            bird.think(pipes)
+            bird.decide()
+            bird.distance = game_score
+            if bird.hits(pipes) == True:
+                bird.dead = True
+                if bird.dead == True:
+                    genome_info.append(bird.genome)
+                    score_info.append(score)
+                    birds.remove(bird)
+                dead_count +=1
+
+
+            if dead_count == len(genomes):
+                return genome_info, score_info
+
+
+
+        clock.tick(10000)
+
+        pygame.display.update()
+        screen.fill((0, 0, 0))
+        game_score +=1
+
+    pygame.quit()
+
