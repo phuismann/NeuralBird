060cc4b9119c49e11302343a4207b31a655d934a
FruitVul <45405932+FruitVul@users.noreply.github.com>
2019-02-10 12:48:02 +0100

    V-0.1
    First version of my flappy bird clone with neat

---
+++ NeuralBird/Main.py
@@ -0,0 +1,76 @@
+import neat
+import Game
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+SCORE = 0
+GENERATION = 0
+MAX_FITNESS = 0
+BEST_GENOME = 0
+
+# def eval_genomes(genomes, config):
+#      i = 0
+#      global SCORE
+#      global MAX_FITNESS
+#      global BEST_GENOME
+#      global GENERATION
+#      GENERATION +=1
+#
+#      for genome_id, genome in genomes:
+#          i = 0
+#          x,y = Game.game(genome,config,SCORE) #game Returns fitness
+#          genome.fitness =y[genome_id]
+#          if genome.fitness >= MAX_FITNESS:
+#              MAX_FITNESS = genome.fitness
+#              BEST_GENOME = genome
+#          SCORE = 0
+#          i += 1
+#          print(MAX_FITNESS,SCORE)
+
+def eval_genomes(genomes, config):
+     # Play game and get results
+     idx, genomes = zip(*genomes) # What does this do?
+
+     genoinf,scoreinf = Game.game(genomes,config,SCORE) #game Returns fitness
+
+
+     # Calculate fitness and top score
+     top_score = 0
+
+     for i, genomes in enumerate(genoinf):
+
+         score = scoreinf[i]
+         fitness = score
+         genomes.fitness = -1 if fitness == 0 else fitness
+         if top_score < score:
+             top_score = score
+
+     # print score
+     print('The top score was:', top_score)
+     fitness_vals.append(fitness)
+
+
+
+
+#Start of actualy program:
+
+fitness_vals = []
+
+
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         'config-feedforward')
+pop = neat.Population(config)
+pop.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+pop.add_reporter(stats)
+
+winner = pop.run(eval_genomes, 100)
+
+x = np.linspace(0,len(fitness_vals),len(fitness_vals))
+sns.lineplot(x=x,y=fitness_vals)
+plt.show()
+
+print(stats)
+
+
---
+++ NeuralBird/Pipe.py
@@ -0,0 +1,32 @@
+import sys, pygame
+import numpy as np
+class pipe:
+
+    size = width, height = 400, 600
+    screen = pygame.display.set_mode(size)
+    ## direct Pipe Parameters
+
+    #top = 0
+    #bottom = 0
+    w = 80
+    speed = 2
+
+    def __init__(self):
+        self.x = self.width  # x position of pipe
+        self.top = np.random.randint(100, self.height / 2)
+        self.bottom = np.random.randint(100, self.height / 3)
+        self.mid_distance = (self.top - self.bottom)*0.5 + self.bottom
+
+    def show(self): #draws two rectangles one top one bot
+        pygame.draw.rect(self.screen,(255,255,255), pygame.Rect(self.x, 0, self.w, self.top)) # Top rect
+        pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(self.x,self.height-self.bottom, self.w, self.bottom)) # Bot rect
+    def update(self):
+        self.x -= self.speed
+
+    def offscreen(self):
+        if self.x < -self.w:
+            return True
+        else:
+            return False
+
+
